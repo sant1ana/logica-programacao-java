632b13f389fc712ae48fd3d16c9bfc7281c25aa5
sant1ana <vanessasantana12@outlook.com>
2024-12-12 22:13:17 -0300

    feat: Adiciona atividade que verifica se um número é par ou ímpar

---
+++ 12-12-2024/VerificadorParOuImpar.java
@@ -0,0 +1,20 @@
+public class VerificadorParOuImpar {
+
+    public static void main(String[] args){
+
+        int numero = 84;
+        int resultado = numero % 2;
+
+        if(resultado == 0){
+            System.out.println("O número é par");
+
+        }else{
+            System.out.println("O número é ímpar");
+
+        }
+
+
+    }
+
+
+}
